5085a1a6b91406c4827308e151b7c994be8559c1
JaretWright <jaret.wright@georgiancollege.ca>
2021-01-20 06:51:21 -0500

    Added Android's JetPack View Vinding

--- app/src/main/java/com/example/w21comp3025lesson2/MainActivity.kt
+++ app/src/main/java/com/example/w21comp3025lesson2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.w21comp3025lesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.w21comp3025lesson2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (binding.passwordEditText.text.toString().equals("1234"))
+            {
+                Snackbar.make(binding.constraintLayout,"Hello ${binding.userNameEditText.text}", Snackbar.LENGTH_INDEFINITE).show()
+            }
+        }
     }
 }
